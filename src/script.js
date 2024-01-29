2fa16e4fb04cff1098fcce90086f4a740580e897
Tetiana Paniot <tatyana.paniot@gmail.com>
2024-01-29 17:03:20 +0000

    base for calculator done

---
+++ src/script.js
@@ -0,0 +1,27 @@
+function calculate() {
+    const firstNumber = parseFloat(document.getElementById('numberInput').value);
+    const secondNumber = parseFloat(document.getElementById('numberTwoInput').value);
+
+    const operation = document.getElementById('operation').value;
+    const result = 0;
+
+    switch (operation) {
+        case 'addition':
+            result = firstNumber + secondNumber;
+            break;
+        case 'substruction':
+            result = firstNumber - secondNumber;
+            break;
+        case 'multiplication':
+            result = firstNumber * secondNumber;
+            break;
+        case 'division':
+            result = firstNumber / secondNumber;
+            break;
+        default:
+            result = `Operation cannot be completed`;
+    }
+
+    document.getElementById('calculationOutcome').innerHTML = "Mathematical operation result: " + result;
+
+}
