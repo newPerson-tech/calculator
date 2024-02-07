1849a783de58c7d561abc74131ba6b1136215da5
Tetiana Paniot <tatyana.paniot@gmail.com>
2024-02-07 10:05:47 +0000

    js code math operation

--- src/script.js
+++ src/script.js
@@ -2,10 +2,13 @@ class Calculator {
     constructor(previousOperandTextEl, currentOperandTextEl) {
         this.previousOperandTextEl = previousOperandTextEl;
         this.currentOperandTextEl = currentOperandTextEl;
+        this.clear();
     }
 
     clear() {
-
+        this.currentOperand = '';
+        this.previousOperand = '';
+        this.operation = undefined;
     }
 
     delete() {
@@ -13,7 +16,8 @@ class Calculator {
     }
 
     appendNumber(number) {
-
+        if (number === '.' && this.currentOperand.includes('.')) return;
+        this.currentOperand = this.currentOperand.toString() + number.toString();
     }
 
     chooseOperation(operation) {
@@ -25,12 +29,8 @@ class Calculator {
     }
 
     updateDisplay() {
-
+        this.currentOperandTextEl.innerText = this.currentOperand;
     }
-
-
-
-
 
 }
 
@@ -42,3 +42,31 @@ const deleteButton = document.querySelector('[data-delete]');
 const allClearButton = document.querySelector('[data-all-clear]');
 const previousOperandTextEl = document.querySelector('[data-previous-operand]');
 const currentOperandTextEl = document.querySelector('[data-current-operand]');
+
+const calculator = new Calculator(previousOperandTextEl, currentOperandTextEl);
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText);
+        calculator.updateDisplay();
+    })
+})
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText);
+        calculator.updateDisplay();
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
